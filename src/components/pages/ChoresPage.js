c41bc6531dfe7106d93dbc5327cab088704b18b0
Tabitha <kadima.t@husky.neu.edu>
2017-10-06 10:10:53 -0400

    new layout for chores management

--- src/components/pages/ChoresPage.js
+++ src/components/pages/ChoresPage.js
@@ -1,68 +1,57 @@
 import React from 'react';
-import Chore from '../elements/Chore';
-
 
 export class ChoresPage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
-        this.state.choresList = [
-                {id: 0, name: "Vacuum", frequency: "Weekly"},
-                {id: 1, name: "Sweep", frequency: "Daily"},
-                {id: 2, name: "Wash dishes", frequency: "Daily"}
-            ]
+        this.state = {
+            task:"",
+            description: "",
+            frequency: "",
+            chores: []
         }
-
-    // find the index of the chore in the list and delete it
-    handleChoreDelete(chore) {
-        var index = this.state.choresList.indexOf(chore);
-        this.state.choresList.splice(index, 1);
-
-        // reset the state
-        this.setState(this.state.choresList);
-    }
-
-    handleChoreAdd() {
-        // needs a unique key
-        var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
-        // this creates a new row
-        var chore = {
-            id: id,
-            name: "",
-            frequency: ""
-        };
-        this.state.choresList.push(chore);
-        // update state
-        this.setState(this.state.choresList);
     }
 
     render() {
-        var self = this;
-        var chore = this.state.choresList.map(function (chore) {
-            return (<Chore chore={chore} onDeleteEvent={self.state.handleChoreDelete.bind(self)} key={chore.id}/> )
-        });
-
         return (
-            <div className="body-content">
-                <table className="table">
-                    <tbody>
-                    {chore}
-                    </tbody>
-                </table>
-                <div className="table-footer btn-group">
-                    <button className="btn btn-info">
-                        <span className="glyphicon glyphicon-floppy-disk"/>
-                        Save
-                    </button>
-                    <button className="btn btn-success pull-right" onClick={this.state.handleChoreAdd}>
-                        <span className="glyphicon glyphicon-plus"/>
-                        Add New
-                    </button>
+            <div className="container">
+                <div className="chore-form col-lg-4 col-md-4 col-sm-4">
+                    <form>
+                        <div className="form-group">
+                            <h4 className="title">Add New Chore</h4>
+                            <label for="choreName">Give a title for the chore</label>
+                            <input className="form-control" type="text" placeholder="e.g. Dishes"/>
+                            <label for="choreName">Describe the task</label>
+                            <textarea className="form-control" type="text"
+                                      placeholder="e.g. load the dishwasher, hand-wash pans"/>
+                            <label for="choreName">How often does it need to be done?</label>
+                            <select className="form-control">
+                                <option>Weekly</option>
+                                <option>Fortnightly</option>
+                                <option>Monthly</option>
+                                <option>Annually</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <div className="chore-list col-lg-8">
+                    <table className="table table-hover">
+                        <thead>
+                        <th>Task</th>
+                        <th>Description</th>
+                        <th>Frequency</th>
+                        <th></th>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Vacuum</td>
+                            <td>vacuum all the carpeted areas in the apartment and empty out the apartment</td>
+                            <td>Weekly</td> </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )
     }
 }
-
-export default ChoresPage;
